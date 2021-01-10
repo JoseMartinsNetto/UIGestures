1a0d49247faa263ea893eb3b72c233deb28443b6
jmsantosnetto <j.msantos.netto@gmail.com>
2021-01-10 17:16:17 -0300

    Implemented Rotation gesture

---
+++ UIGestures/Controllers/RotationGestureViewController.swift
@@ -0,0 +1,46 @@
+//
+//  RotationGestureViewController.swift
+//  UIGestures
+//
+//  Created by Jose Martins on 10/01/21.
+//
+
+import UIKit
+
+class RotationGestureViewController: UIViewController {
+    @IBOutlet var vwRotateMe: UIView!
+    //-----------------------------------------------------------------------
+    //    MARK: - UIViewController
+    //-----------------------------------------------------------------------
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configGestures()
+    }
+    
+    //-----------------------------------------------------------------------
+    //    MARK: - CustomMethods
+    //-----------------------------------------------------------------------
+    
+    func configGestures() {
+        let rotationGesture = UIRotationGestureRecognizer(target: self, action: #selector(handleRotationGesture))
+        
+        vwRotateMe.addGestureRecognizer(rotationGesture)
+    }
+    
+    @objc func handleRotationGesture(_ sender: UIRotationGestureRecognizer) {
+        
+        if let view = sender.view {
+            
+            view.transform = view.transform.rotated(by: sender.rotation)
+            sender.rotation = 0
+            
+            if sender.state == .ended {
+                UIView.animate(withDuration: 0.3) {
+                    view.transform = CGAffineTransform(rotationAngle: 0)
+                }
+            }
+            
+        }
+    }
+}
